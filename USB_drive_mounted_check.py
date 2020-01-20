9552871eacba25893774adb292e3092ec66642e8
David Metcalfe <DavidMetcalfe@users.noreply.github.com>
2020-01-20 12:43:17 -0800

    Adding detail to comments

--- USB_drive_mounted_check.py
+++ USB_drive_mounted_check.py
@@ -2,7 +2,9 @@
 # Loops through available drives and returns 
 # the drive letter if one matches the given
 # drive name. If none match, return 0.
-# 
+# Ideal for checking if a specific USB 
+# drive is mounted. 
+#
 # David Metcalfe, January 19, 2020
 # ------------------------------------
 
